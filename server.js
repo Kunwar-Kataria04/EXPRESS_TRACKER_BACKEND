49c857329e2807e9ba655196d36d41145662d874
Kunwar-Kataria04 <138360875+Kunwar-Kataria04@users.noreply.github.com>
2025-08-13 02:55:27 -0700

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const dashboardRoutes = require("./routes/dashboardRoutes");
 
 app.use(
     cors({
-        origin: process.env.CLIENT_URL,
+        origin: https://expense-tracker-kunwar.netlify.app/,
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
     })
